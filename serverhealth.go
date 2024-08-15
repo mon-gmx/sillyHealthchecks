bfec0a85a9220a2e5d085a5b8922c7b5ce06c072
mon <ramonfierrogarcia@gmail.com>
2024-08-14 22:21:26 -0600

    removing channels and unnecessary checks

--- serverhealth.go
+++ serverhealth.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	//"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"net/url"
 	"strings"
 	"sync"
 )
 
 type WebsiteChecker func(string) bool
 
-type HostMetrics struct {
-	url    string
-	status bool
-}
-
 type Response struct {
 	Metric   string `json: metric`
 	Hostname string `json: hostname`
-	Status   string `json: status`
+	Status   int `json: status`
 }
 
 var handlerMux sync.Mutex
@@ -33,71 +25,46 @@ func HealthCheck(url string) bool {
 	return response.StatusCode == http.StatusOK
 }
 
-func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
-	results := make(map[string]bool)
-	resultChannel := make(chan HostMetrics)
-
-	for _, url_ := range urls {
-		go func(u string) {
-			u = "http://" + u
-			parsedURL, err := url.Parse(u)
-			if err == nil {
-				hostname := parsedURL.Hostname()
-				log.Printf("hostname: %v, url: %v", hostname, u)
-				resultChannel <- HostMetrics{url: hostname, status: wc(u)}
-			}
-		}(url_)
-
+func DoHealthCheck(wc WebsiteChecker, hostname string) int {
+	healthcheckMap := map[string]string{
+        	"berryone": "http://192.168.0.110/admin/index.php",
+	        "berrytwo": "http://192.168.0.120",
+        	"berrythree": "http://192.168.0.130",
+	        "berryfour": "http://192.168.0.140",
 	}
 
-	for i := 0; i < len(urls); i++ {
-		r := <-resultChannel
-		results[r.url] = r.status
+	url, present := healthcheckMap[hostname]
+	r := false
+	if present {
+		r = wc(url)
+	} else {
+		return -1
 	}
-	return results
+	if !r {
+		return 0
+	} else {
+		return 1
+	}
 }
 
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	host := strings.TrimPrefix(r.URL.Path, "/health/")
-	log.Println(r)
+	hostname := strings.TrimPrefix(r.URL.Path, "/health/")
 	if r.Method == http.MethodGet {
-		handleGetPost(w, host)
+		handleGetPost(w, hostname)
 	} else {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
-func handleGetPost(w http.ResponseWriter, host string) {
+func handleGetPost(w http.ResponseWriter, hostname string) {
 	handlerMux.Lock()
 	defer handlerMux.Unlock()
-	status := "bad"
-
-	urls := []string{
-		"192.168.0.110/admin/index.php",
-		"192.168.0.120",
-		"192.168.0.130",
-		"192.168.0.140",
-	}
-	healthMap := CheckWebsites(HealthCheck, urls)
-	health, ok := healthMap[host]
-	if !ok {
+	health := DoHealthCheck(HealthCheck, hostname)
+	if health < 0 {
 		http.Error(w, "Host not found", http.StatusNotFound)
-	}
-	if health {
-		status = "ok"
+		return
 	}
 
-	/*
-		response := Response{
-			Hostname: host,
-			Status:   status,
-			Metric:   "healtcheck",
-		}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(response)
-	*/
-
-	// formatting as prometheus response
-	stringResponse := fmt.Sprintf("healthcheck{host=%q} %v", host, status)
+	stringResponse := fmt.Sprintf("healthcheck{hostname=%q} %v", hostname, health)
 	w.Write([]byte(stringResponse))
 }
